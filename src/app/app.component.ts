933487aceb545a7f5cad16d2991ca693206ee7b8
Bruno C. Manso <mrbrunomanso@gmail.com>
2023-03-12 18:03:28 -0300

    remove on expiration fixture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { AuthGuard } from './guards/auth.guard';
 import { LoadingGuard } from './guards/loading.guard';
+import jwt_decode from 'jwt-decode';
+
 
 @Component({
   selector: 'app-root',
@@ -11,16 +12,31 @@ export class AppComponent {
   public progress: number = 0;
   showSplashScreen = true;
 
-  constructor(private loadingGuard: LoadingGuard) {}
+
+  constructor(private loadingGuard: LoadingGuard) { }
 
   ngOnInit() {
+
     this.loadingGuard.canActivate().then(() => {
       this.showSplashScreen = false;
     });
 
-    
+    this.checkTokenExpiration();
   }
 
+  checkTokenExpiration() {
+    const token = localStorage.getItem('jwtToken');
+    if (token) {
+      const decodedToken = jwt_decode(token) as DecodedToken;
+      if (decodedToken.exp < Date.now() / 1000) {
+        localStorage.removeItem('jwtToken');
+      }
+    }
+  }
+}
 
-
+interface DecodedToken {
+  sub: string;
+  exp: number;
+  iat: number;
 }
